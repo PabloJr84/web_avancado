26cb136a4203bb181856871f2219c7a4ce23208a
Pablo Assis <pablo.assis1984@gmail.com>
2021-10-15 00:11:09 -0300

    Curso Web Avançado

---
+++ inicio/comentario.js
@@ -0,0 +1,13 @@
+//console.log é usado para exibir algo na tela do computador!
+console.log("Passo #01");
+console.log("Passo #02");//mais um exemplo de console.log
+console.log("Passo #03");
+/*
+Objeto Console
+O consoleé um objeto com diversos métodos associados.
+O objeto consoleoferece acesso à consola de depuração do navegador.
+ O funcionamento do procedimento varia de navegador para navegador mas existem métodos que são vistos como um standard. Um desses métodos é o log().*/ 
+  
+ /*Log do método
+O método log()existe essencialmente para permitir o envio de dados para a consola de depuração do navegador. 
+Pode ser qualquer informação, normalmente com o intuito de código depurar.*/
---
+++ inicio/sentenca_1.js
@@ -0,0 +1,5 @@
+console.log(1+3);
+console.log("Pablo Junior Moreira de Assis");
+console.log("Passo #01");
+console.log("Passo #02");
+console.log("Passo #03");
